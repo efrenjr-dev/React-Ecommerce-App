928f30be0306bc0a9d47abfb3f26ec067ea4796d
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-06-03 17:06:11 +0800

    view products admin view

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,45 +1,88 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Card from "react-bootstrap/Card";
 import Product from "../components/Product";
 import { Spinner } from "react-bootstrap";
+import { UserContext } from "../userContext";
 
 export default function Products() {
+    const { user } = useContext(UserContext);
     const [isLoading, setIsLoading] = useState(true);
     const [activeProducts, setActiveProducts] = useState([]);
 
     useEffect(() => {
-        setIsLoading(true);
-        fetch("https://e-commerce-api-2.vercel.app/products/", {
-            method: "GET",
-            mode: "cors",
-            headers: {
-                "Content-Type": "application/json",
-            },
-        })
-            .then((result) => result.json())
-            .then((data) => {
-                // console.log(data);
-                setActiveProducts(
-                    data.map((product) => {
-                        // console.log(product);
-                        return (
-                            <Col
-                                className="d-flex justify-content-center align-items-stretch"
-                                key={product._id}
-                            >
-                                <Product productProp={product} />
-                            </Col>
-                        );
-                    })
-                );
-                setIsLoading(false);
+        if (!user.isAdmin) {
+            setIsLoading(true);
+            fetch("https://e-commerce-api-2.vercel.app/products/", {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem(
+                        "ecommercetoken"
+                    )}`,
+                },
             })
-            .catch((error) => {
-                return `ERROR! ${error}`;
-            });
+                .then((result) => result.json())
+                .then((data) => {
+                    // console.log(data);
+                    setActiveProducts(
+                        data.map((product) => {
+                            // console.log(product);
+                            return (
+                                <Col
+                                    className="d-flex justify-content-center align-items-stretch"
+                                    key={product._id}
+                                >
+                                    <Product productProp={product} />
+                                </Col>
+                            );
+                        })
+                    );
+                    setIsLoading(false);
+                })
+                .catch((error) => {
+                    return `ERROR! ${error}`;
+                });
+        }
     }, []);
+
+    useEffect(() => {
+        if (user.isAdmin) {
+            setIsLoading(true);
+            fetch("https://e-commerce-api-2.vercel.app/products/all", {
+                method: "GET",
+                mode: "cors",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${localStorage.getItem(
+                        "ecommercetoken"
+                    )}`,
+                },
+            })
+                .then((result) => result.json())
+                .then((data) => {
+                    // console.log(data);
+                    setActiveProducts(
+                        data.map((product) => {
+                            // console.log(product);
+                            return (
+                                <Col
+                                    className="d-flex justify-content-center align-items-stretch"
+                                    key={product._id}
+                                >
+                                    <Product productProp={product} />
+                                </Col>
+                            );
+                        })
+                    );
+                    setIsLoading(false);
+                })
+                .catch((error) => {
+                    return `ERROR! ${error}`;
+                });
+        }
+    }, [user]);
 
     return isLoading ? (
         <Row className="text-center mt-5">
