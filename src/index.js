fbced60266215390fadfe30dbdbc67d9871b5751
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-08-01 19:37:50 +0800

    react format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 // import './index.scss';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App";
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
-
-
